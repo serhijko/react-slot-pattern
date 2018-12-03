875c0b9b128b98ee5ec309e86aba1a24ccd109a6
serhijko <serhijko>
2018-12-03 21:54:45 +0300

    state as props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,28 @@ import './App.css';
 import Greeting from './components/Greeting';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isShow: true,
+    };
+  }
+
+  toggleShow = () => {
+    this.setState(state => ({ isShow: !state.isShow }));
+  };
+
   render() {
+    const greeting = 'Welcome to React';
+    
     return (
       <div className="App">
-        <Greeting greeting={{ text: 'Welcome to React' }} />
+        <Greeting greeting={greeting} isShow={this.state.isShow} />
+
+        <button onClick={this.toggleShow} type="button">
+          Toggle Show
+        </button>
       </div>
     );
   }
